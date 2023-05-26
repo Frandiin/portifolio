ab0cecbc1011eb6b18b4d0dd4b5561ecb243f0b0
Frandim <wellingtonfrandin@hotmail.com>
2023-05-25 23:16:36 -0300

    icon

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,11 @@ export default defineConfig({
             sizes: "512x512",
             type: "image/png",
           },
+          {
+            src: "/w-button-icon.png",
+            sizes: "128x128",
+            type: "image/png",
+          },
         ],
       },
     }),
